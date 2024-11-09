94d4ea8b0d81afafdd3bdc195cccd160ad083f77
Anoop Patel <anooppatelmzpp@gmail.com>
2024-11-09 11:27:05 +0530

    Update : desktop navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -15,37 +15,48 @@ const Navbar = () => {
       <img src={logo} alt="logo" className="logo" onClick={() => handleNavigation("/")} />
       
       <div className="desktop-menu">
-        <span
-          className={`menu-name ${location.pathname === "/" ? "active" : ""}`}
-          onClick={() => handleNavigation("/")}
-        >
-          Home
-        </span>
-        <span
-          className={`menu-name ${location.pathname === "/about" ? "active" : ""}`}
-          onClick={() => handleNavigation("/about")}
-        >
-          About
-        </span>
-        <span
-          className={`menu-name ${location.pathname === "/service" ? "active" : ""}`}
-          onClick={() => handleNavigation("/service")}
-        >
-          Service
-        </span>
-        <span
-          className={`menu-name ${location.pathname === "/demo" ? "active" : ""}`}
-          onClick={() => handleNavigation("/demo")}
-        >
-          Demo
-        </span>
-        <span
-          className={`menu-name ${location.pathname === "/event" ? "active" : ""}`}
-          onClick={() => handleNavigation("/event")}
-        >
-          Event
-        </span>
-      </div>
+  <div className="item">
+    <span
+      className={`menu-name ${location.pathname === "/" ? "active" : ""}`}
+      onClick={() => handleNavigation("/")}
+    >
+      Home
+    </span>
+  </div>
+  <div className="item">
+    <span
+      className={`menu-name ${location.pathname === "/about" ? "active" : ""}`}
+      onClick={() => handleNavigation("/about")}
+    >
+      About
+    </span>
+  </div>
+  <div className="item">
+    <span
+      className={`menu-name ${location.pathname === "/service" ? "active" : ""}`}
+      onClick={() => handleNavigation("/service")}
+    >
+      Service
+    </span>
+  </div>
+  <div className="item">
+    <span
+      className={`menu-name ${location.pathname === "/demo" ? "active" : ""}`}
+      onClick={() => handleNavigation("/demo")}
+    >
+      Demo
+    </span>
+  </div>
+  <div className="item">
+    <span
+      className={`menu-name ${location.pathname === "/event" ? "active" : ""}`}
+      onClick={() => handleNavigation("/event")}
+    >
+      Event
+    </span>
+  </div>
+     </div>
+
     </div>
   );
 };
